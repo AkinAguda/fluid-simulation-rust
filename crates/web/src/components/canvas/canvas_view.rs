635fe0217ba6e23b2691d1ee1f6fab14fcb0f6cf
Akin Aguda <lordtitan7@gmail.com>
2022-08-12 01:06:07 +0100

    now setting canvas dimensions

--- crates/web/src/components/canvas/canvas_view.rs
+++ crates/web/src/components/canvas/canvas_view.rs
@@ -2,7 +2,7 @@ use std::rc::Rc;
 
 use percy_dom::*;
 
-use crate::log;
+use crate::utility::functions::{get_display_dimensions, resize_canvas_to_display_size};
 
 pub struct Canvas {
     pub set_fluid_size: Rc<dyn Fn(u16, u16) -> ()>,
@@ -11,12 +11,16 @@ pub struct Canvas {
 impl View for Canvas {
     fn render(&self) -> VirtualNode {
         let css = css_mod::get!("canvas.css");
+        let set_fluid_size = self.set_fluid_size.clone();
         html! {
             <canvas
                 key="main-canvas"
                 class=css["canvas"]
-                on_create_element = move |element: web_sys::Element| {
-                    log("CREATED");
+                on_create_element = move |canvas: web_sys::Element| {
+                    let canvas = canvas.dyn_into::<web_sys::HtmlCanvasElement>().unwrap();
+                    resize_canvas_to_display_size(&canvas);
+                    let (width, height) = get_display_dimensions(canvas.width(), canvas.height());
+                    (set_fluid_size)(width as u16, height as u16);
                 }
             ></canvas>
         }
--- crates/web/src/utility/functions.rs
+++ crates/web/src/utility/functions.rs
@@ -1,6 +1,6 @@
 use num_traits::ToPrimitive;
 
-pub fn resize_canvas_to_display_size(canvas: web_sys::HtmlCanvasElement) -> bool {
+pub fn resize_canvas_to_display_size(canvas: &web_sys::HtmlCanvasElement) -> bool {
     let window = web_sys::window().unwrap();
     let dpr = window.device_pixel_ratio();
     let dom_rect = canvas.get_bounding_client_rect();
@@ -16,4 +16,19 @@ pub fn resize_canvas_to_display_size(canvas: web_sys::HtmlCanvasElement) -> bool
     }
 
     need_resize
+}
+
+pub fn get_display_dimensions(width: u32, height: u32) -> (u32, u32) {
+    let mut count: f32 = 220.0;
+    let div = (u32::max(width, height) / u32::min(width, height)) as f32;
+    if div as f32 <= 1.5 {
+        count = 180.0;
+    }
+    if width > height {
+        (count as u32, (count / (width / height) as f32) as u32)
+    } else if height > width {
+        ((count / (height / width) as f32) as u32, count as u32)
+    } else {
+        (width, height)
+    }
 }
