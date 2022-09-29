60b220dbcfe67fb2fdfe8e2a8438141b98397a98
Akin Aguda <lordtitan7@gmail.com>
2022-09-29 02:09:40 +0100

    Added touch events

--- crates/web/src/components/canvas/canvas_view.rs
+++ crates/web/src/components/canvas/canvas_view.rs
@@ -26,9 +26,14 @@ impl View for Canvas {
         let mouse_state_ref_1 = self.data.mouse_state.clone();
         let mouse_state_ref_2 = self.data.mouse_state.clone();
         let mouse_state_ref_3 = self.data.mouse_state.clone();
+        let mouse_state_ref_4 = self.data.mouse_state.clone();
+        let mouse_state_ref_5 = self.data.mouse_state.clone();
+        let mouse_state_ref_6 = self.data.mouse_state.clone();
+        let mouse_state_ref_7 = self.data.mouse_state.clone();
 
         let add_properties_from_mouse_loc_ref_1 = self.data.add_properties_from_mouse_loc.clone();
         let add_properties_from_mouse_loc_ref_2 = self.data.add_properties_from_mouse_loc.clone();
+        let add_properties_from_mouse_loc_ref_3 = self.data.add_properties_from_mouse_loc.clone();
 
         let onclick = move |event: web_sys::MouseEvent| {
             (add_properties_from_mouse_loc_ref_1)(get_client_values(InputEvents::Mouse(event)));
@@ -50,6 +55,26 @@ impl View for Canvas {
             }
         };
 
+        let touchstart = move || {
+            mouse_state_ref_4.borrow_mut().mouse_down = true;
+        };
+
+        let touchend = move || {
+            mouse_state_ref_5.borrow_mut().reset();
+        };
+
+        let touchcancel = move || {
+            mouse_state_ref_6.borrow_mut().reset();
+        };
+
+        let touchmove = move |event: web_sys::TouchEvent| {
+            let md = mouse_state_ref_7.borrow().mouse_down;
+            if md {
+                mouse_state_ref_7.borrow_mut().mouse_down = true;
+                (add_properties_from_mouse_loc_ref_3)(get_client_values(InputEvents::Touch(event)));
+            }
+        };
+
         html! {
             <canvas
                 id=CANVAS_ID
@@ -66,6 +91,18 @@ impl View for Canvas {
                 }
                 onmouseup=move || {
                     (onmouseup)();
+                }
+                ontouchstart=move || {
+                    (touchstart)();
+                }
+                ontouchend=move || {
+                    (touchend)();
+                }
+                ontouchcancel=move || {
+                    (touchcancel)();
+                }
+                ontouchmove=move |event: web_sys::TouchEvent| {
+                    (touchmove)(event);
                 }
             >
             /canvas>
