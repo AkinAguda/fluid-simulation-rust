8e272d7a4dd2c495edd4208faee79eacef153461
Akin Aguda <lordtitan7@gmail.com>
2022-09-29 23:39:27 +0100

    even another attempt

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
 echo -e "\e[1;33m BUILDING... Installing and setting up cargo \e[0m"
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-rustup install stable &&
-rustup default stable &&
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh &&
 echo -e "\e[1;32m BUILDING... Cargo Installed sucessfully \e[0m"
-
-echo -e "\e[1;33m BUILDING... Installing wasm-pack \e[0m"
-curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh &&
-echo -e "\e[1;32m BUILDING... wasm-pack Installed sucessfully \e[0m"
 
 echo -e "\e[1;33m BUILDING... Installing wasm-bindgen \e[0m"
 cargo install -f wasm-bindgen-cli &&
