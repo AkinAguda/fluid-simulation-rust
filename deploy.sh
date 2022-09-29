ddb5360578edc6bbc3f571a4b0975fa7d0ab5e56
Akin Aguda <lordtitan7@gmail.com>
2022-09-29 23:15:07 +0100

    adjustment

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,10 @@ rustup install stable &&
 rustup default stable &&
 echo -e "\e[1;32m BUILDING... Cargo Installed sucessfully \e[0m"
 
+echo -e "\e[1;33m BUILDING... Installing wasm-pack \e[0m"
+curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh &&
+echo -e "\e[1;32m BUILDING... wasm-pack Installed sucessfully \e[0m"
+
 echo -e "\e[1;33m BUILDING... Installing wasm-bindgen \e[0m"
 cargo install -f wasm-bindgen-cli &&
 echo -e "\e[1;32m BUILDING... wasm-bindgen Installed sucessfully \e[0m"
