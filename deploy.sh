38fb801bf686ef8ce33ac724b162590fefbfcc72
Akin Aguda <lordtitan7@gmail.com>
2022-11-19 14:35:23 +0100

    should correctly deploy build err

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,12 @@
 
 echo -e "\e[1;33m BUILDING... Installing and setting up cargo \e[0m"
 curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain none &&
-echo -e "\e[1;32m BUILDING... Cargo Installed sucessfully \e[0m"
+echo -e "\e[1;32m BUILDING... Cargo Installed sucessfully \e[0m" &&
 
-echo -e "\e[1;33m BUILDING... Installing wasm-bindgen \e[0m"
+echo -e "\e[1;33m BUILDING... Installing wasm-bindgen \e[0m" &&
 cargo install -f wasm-bindgen-cli &&
-echo -e "\e[1;32m BUILDING... wasm-bindgen Installed sucessfully \e[0m"
+echo -e "\e[1;32m BUILDING... wasm-bindgen Installed sucessfully \e[0m" &&
 
-rustup target add wasm32-unknown-unknown
+rustup target add wasm32-unknown-unknown &&
 
 ./build.sh
