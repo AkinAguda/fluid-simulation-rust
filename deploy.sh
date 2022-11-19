fbf40191dbc90772d5a35ac4423e03b167fbce9a
Akin Aguda <lordtitan7@gmail.com>
2022-11-19 01:23:11 +0100

    updated rust version

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo -e "\e[1;33m BUILDING... Installing and setting up cargo \e[0m"
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh &&
+curl https://sh.rustup.rs -sSf | sh &&
 echo -e "\e[1;32m BUILDING... Cargo Installed sucessfully \e[0m"
 
 echo -e "\e[1;33m BUILDING... Installing wasm-bindgen \e[0m"
