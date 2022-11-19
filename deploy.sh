3d17a2f149855ef5fb79bc1141e2f2fd02a8c4cf
Akin Aguda <lordtitan7@gmail.com>
2022-11-19 01:24:41 +0100

    should fix deploy error

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo -e "\e[1;33m BUILDING... Installing and setting up cargo \e[0m"
-curl https://sh.rustup.rs -sSf | sh &&
+curl https://sh.rustup.rs -sSf | sh && rustup default stable &&
 echo -e "\e[1;32m BUILDING... Cargo Installed sucessfully \e[0m"
 
 echo -e "\e[1;33m BUILDING... Installing wasm-bindgen \e[0m"
