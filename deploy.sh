ddce84df89de807f6858a9ba455e5ccd4bb168f0
Akin Aguda <lordtitan7@gmail.com>
2022-11-19 14:29:01 +0100

    fix attempt 6

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo -e "\e[1;33m BUILDING... Installing and setting up cargo \e[0m"
-yes | curl https://sh.rustup.rs -sSf | sh  &&
+curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain none &&
 echo -e "\e[1;32m BUILDING... Cargo Installed sucessfully \e[0m"
 
 echo -e "\e[1;33m BUILDING... Installing wasm-bindgen \e[0m"
