cdb8544137ec9f52af658a672a803321ebb3d2b1
Akin Aguda <lordtitan7@gmail.com>
2022-11-19 15:16:08 +0100

    fix 7

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-echo -e "\e[1;33m BUILDING... Installing and setting up cargo \e[0m"
-curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain none &&
+echo -e "\e[1;33m BUILDING... Installing and setting up cargo \e[0m" &&
+
+curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain -y none &&
+
 echo -e "\e[1;32m BUILDING... Cargo Installed sucessfully \e[0m" &&
 
 echo -e "\e[1;33m BUILDING... Installing wasm-bindgen \e[0m" &&
+
 cargo install -f wasm-bindgen-cli &&
+
 echo -e "\e[1;32m BUILDING... wasm-bindgen Installed sucessfully \e[0m" &&
 
 rustup target add wasm32-unknown-unknown &&
