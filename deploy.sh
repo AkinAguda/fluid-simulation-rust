947f2f913435082195780bc2febbe47678a10296
Akin Aguda <lordtitan7@gmail.com>
2022-11-19 01:59:26 +0100

    attempt 4

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo -e "\e[1;33m BUILDING... Installing and setting up cargo \e[0m"
-curl https://sh.rustup.rs -sSf | sh -y &&
+curl https://sh.rustup.rs -sSf | sh &&
 echo -e "\e[1;32m BUILDING... Cargo Installed sucessfully \e[0m"
 
 echo -e "\e[1;33m BUILDING... Installing wasm-bindgen \e[0m"
